0b08a01b5917bce6a43d12d618050c0d8c05ac5f
itzwe <itzwewewe@naver.com>
2023-06-03 21:17:46 +0900

    주어진 수에서 최솟값 구하기

--- Lecture/Section1/최솟값구하기.js
+++ Lecture/Section1/최솟값구하기.js
@@ -1,12 +1,30 @@
-function solution(a, b, c){
-  let answer;
-  if(a>b){
-    answer = b;
-  }
-  else answer =a;
-  if(c<answer) answer = c;
 
-  return answer ;
+// 주어진 수에서 가장 최소 값 구하기
+function solution(arr){         
+  let answer = arr[0] ;
+  for(let i=1; i<arr.length; i++){
+    if(answer> arr[i]) answer = arr[i]
+  }
+  return answer;
+
 }
 
-console.log(solution(2, 5, 1));
+let arr=[5, 7, 1, 3, 2, 9, 11];
+console.log(solution(arr));
+
+
+// let answer = Math.min(...arr); 방법도 있음.
+
+
+// function solution(arr){         
+//   let answer, min=Number.MAX_SAFE_INTEGER; => 큰 수를 min에 초기화함. 
+//   for(let i=0; i<arr.length; i++){
+//       if(arr[i]<min) min=arr[i];
+//   }
+//   answer=min;
+//   return answer;
+
+// }
+
+// let arr=[5, 7, 1, 3, 2, 9, 11];
+// console.log(solution(arr));
