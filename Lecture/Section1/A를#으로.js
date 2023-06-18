e7f6bdbe2106b74d01d384b07ca0ea521bb67ff3
itzwe <itzwewewe@naver.com>
2023-06-18 18:21:30 +0900

    문자열 변환

---
+++ Lecture/Section1/A를#으로.js
@@ -0,0 +1,10 @@
+// 대문자로 이루어진 영어단어가 입력되면 단어에 포함된 ‘A'를 모두 ’#‘으로 바꾸어 출력하는 
+// 프로그램을 작성하세요.
+
+function solution(s){
+  let answer= s.replaceAll('A', '#');
+  return answer;
+}
+
+let str="BANANASDWAASDSCAA";
+console.log(solution(str));
