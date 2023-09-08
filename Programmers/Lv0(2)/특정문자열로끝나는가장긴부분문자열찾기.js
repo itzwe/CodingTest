c90c6f338af5cf4e590153ccc6df8580e664ace3
itzwe <itzwewewe@naver.com>
2023-09-08 21:47:42 +0900

    긴부분 문자열 찾기 84%

---
+++ Programmers/Lv0(2)/특정문자열로끝나는가장긴부분문자열찾기.js
@@ -0,0 +1,12 @@
+// 문자열 myString과 pat가 주어집니다. myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열
+// 을 찾아서 return 하는 solution 함수를 완성해 주세요.
+
+function solution(s, p) {
+  var answer = '';
+  for(let i=0; i<s.length; i++){
+      let str = s.substr(0,i+1);
+      if( str.endsWith(p)) answer = str;
+  }
+  
+  return answer;
+}
