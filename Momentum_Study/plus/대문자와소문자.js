f1c71342dfa5fca0204a9e9a8e213e5fa52ae344
itzwe <itzwewewe@naver.com>
2023-09-26 20:45:06 +0900

    9월 26일 추가 풀이

---
+++ Momentum_Study/plus/대문자와소문자.js
@@ -0,0 +1,9 @@
+// 문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는
+//  대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+function solution(my_string) {
+  var answer = '';
+  for(let x of my_string){
+      x === x.toLowerCase() ? answer += x.toUpperCase() : answer += x.toLowerCase()
+  }
+  return answer;
+}
